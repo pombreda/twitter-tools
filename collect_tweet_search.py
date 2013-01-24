d77fb82ca106e60820c974cd877ade71af7a7b78
Ramiro Gomez <web@ramiro.org>
2013-01-24 01:11:36 +0100

    added tweet collector that stores tweets in json files

--- collect_tweet_search.py
+++ collect_tweet_search.py
@@ -8,9 +8,16 @@
 # file that contain all found tweets.
 #
 # Tweet ids are stored in a dict and tweets in a list.
+# FIXME this code is a quick ugly hack
 
-import sys, re, tweepy
+import os, sys, re, json, ConfigParser, tweepy
 
+def get_data_from_file(filename, default):
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            return json.load(f)
+    return default
+        
 if 2 != len(sys.argv):
     print('Usage\n%s "search term"' % sys.argv[0])
     sys.exit()
@@ -19,5 +26,28 @@ search_term = sys.argv[1].strip().lower()
 basename = unicode(re.sub('[^\w-]', '-', search_term))
 id_file = basename + '_ids.json'
 tweet_file = basename + '_tweets.json'
+ids = get_data_from_file(id_file, {})
+tweets = get_data_from_file(tweet_file, [])
 
-print (id_file, tweet_file)
+config = ConfigParser.ConfigParser()
+config.readfp(open('collect_tweet_search.cfg'))
+
+auth = tweepy.OAuthHandler(config.get('twitter', 'CONSUMER_KEY'), config.get('twitter', 'CONSUMER_SECRET'))
+auth.set_access_token(config.get('twitter', 'ACCESS_TOKEN'), config.get('twitter', 'ACCESS_TOKEN_SECRET'))
+api = tweepy.API(auth)
+
+def proc_tweets():
+    for page in tweepy.Cursor(api.search, search_term, include_entities=1, count=200, include_rts=True).pages():
+        for tweet in page:
+            # must use id string because of json encoding
+            if tweet.id_str in ids:
+                return # suppose all subsequent tweets have been seen already
+            ids[tweet.id_str] = 1
+            td = tweet.__dict__
+            td['created_at'] = str(td['created_at'])
+            tweets.append(td)
+
+proc_tweets()
+
+with open(id_file, 'w') as f: json.dump(ids, f)
+with open(tweet_file, 'w') as f: json.dump(tweets, f)
---
+++ collect_tweet_search_check.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+import json
+print len(json.load(open('ivmooc_ids.json', 'r')))
+print len(json.load(open('ivmooc_tweets.json', 'r')))
